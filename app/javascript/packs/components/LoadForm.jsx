4a0e644a42fae622f3180caa490d44a47fe99649
Alexander Kurakov <zmshurik@yandex.ru>
2019-05-29 19:44:30 +0700

    add handlers in LoadForm

--- app/javascript/packs/components/LoadForm.jsx
+++ app/javascript/packs/components/LoadForm.jsx
@@ -1,35 +1,79 @@
 import React from 'react'
 
 export default class LoadForm extends React.Component {
+
+  state = {
+    owner: '',
+    repo: '',
+    author: '',
+  };
+
+  handleOwnerChange = ({ target: { value } }) => {
+    this.setState({ owner: value });
+  }
+
+  handleRepoChange = ({ target: { value } }) => {
+    this.setState({ repo: value });
+  }
+
+  handleAuthorChange = ({ target: { value } }) => {
+    this.setState({ author: value });
+  }
+
+  handleCommitsLoad = e => {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <form className="form-inline">
-        <label className="sr-only" for="owner">Owner</label>
+      <form className="form-inline" onSubmit={ this.handleCommitsLoad }>
+        <label className="sr-only" htmlFor="owner">Owner</label>
         <div className="input-group mb-2 mr-sm-2">
           <div className="input-group-prepend">
             <div className="input-group-text">></div>
           </div>
-          <input type="text" className="form-control" id="owner" placeholder="Owner" />
+          <input
+            type="text"
+            className="form-control"
+            id="owner"
+            placeholder="Owner"
+            onChange={ this.handleOwnerChange }
+            value={ this.state.owner }
+          />
         </div>
 
-        <label className="sr-only" for="repo">Repository</label>
+        <label className="sr-only" htmlFor="repo">Repository</label>
         <div className="input-group mb-2 mr-sm-2">
           <div className="input-group-prepend">
             <div className="input-group-text">></div>
           </div>
-          <input type="text" className="form-control" id="repo" placeholder="Repository" />
+          <input
+            type="text"
+            className="form-control"
+            id="repo"
+            placeholder="Repository"
+            onChange={ this.handleRepoChange }
+            value={ this.state.repo }
+          />
         </div>
 
-        <label className="sr-only" for="author_email">Author email</label>
+        <label className="sr-only" htmlFor="author">Author</label>
         <div className="input-group mb-2 mr-sm-2">
           <div className="input-group-prepend">
             <div className="input-group-text">></div>
           </div>
-          <input type="text" className="form-control" id="author_email" placeholder="Author email" />
+          <input 
+            type="text"
+            className="form-control"
+            id="author"
+            placeholder="Author email or login"
+            onChange={ this.handleAuthorChange }
+            value={ this.state.author }
+          />
         </div>
 
         <button type="submit" className="btn btn-primary mb-2">Load</button>
       </form>
     )
-  }
+  };
 }
