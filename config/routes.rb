28af15042bad995f52caf502a75b878cf70d02f1
Alexander Kurakov <zmshurik@yandex.ru>
2019-06-03 16:31:58 +0700

    del whitespase

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root :to => 'main_page#show'
   resources :commits, only: [:index, :create]
   delete '/commits' => 'commits#destroy', as: :group_delete
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
